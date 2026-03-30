abb00ce29473943fdc7fce902b0c8dcffb801f81
dipangshu22 <dipangshu22@gmail.com>
2026-03-30 19:58:53 +0530

    next

--- app.py
+++ app.py
@@ -147,5 +147,7 @@ def run_news():
 # ==============================
 # RUN SERVER
 # ==============================
+
+port = int(os.environ.get("PORT", 10000))
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=port)
